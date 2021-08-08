4c90902fc4430d4a713311542211ddc674162fbe
Michael Eisenbraun <=>
2021-08-08 00:51:20 -0400

    Adds tests for resizeImage

--- src/tests/utilitiesSpec.ts
+++ src/tests/utilitiesSpec.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import {promises as fsPromises} from 'fs';
 import utilities from "../utilities";
 
 
@@ -7,17 +8,17 @@ describe('testing fullImagePath', () => {
   const goodFile = 'fjord'
   const badFile = 'no-file'
 
-  it('fullImagePath should return the correct path string', () => {
+  it('expects fullImagePath(goodFile) to be value', () => {
     expect(utilities.fullImagePath(goodFile)).toBe(`${process.cwd()}/assets/full/${goodFile}.jpg`)
   })
 
-  it('fullImagePath should return a path to a valid file', () => {
+  it('expects fullImagePath(goodFile) to be a valid file', () => {
     fs.access(utilities.fullImagePath(goodFile), fs.constants.F_OK, (err) => {
       expect(err).toBeFalsy()
     })
   })
 
-  it('fullImagePath should return a path to a invalid file', () => {
+  it('expects fullImagePath(badFile) to not be a valid file', () => {
     fs.access(utilities.fullImagePath(badFile), fs.constants.F_OK, (err) => {
       expect(err).toBeTruthy()
     })
@@ -31,31 +32,103 @@ describe('testing thumbImagePath', () => {
   const heightNumber = 200
   const heightString = '200'
 
-  it('thumbImagePath should take numbers and return a path with a width and height', () => {
+  it('expects thumbImagePath(filename, widthNumber, heightNumber) to be value', () => {
     expect(utilities.thumbImagePath(filename, widthNumber, heightNumber)).toBe(`${process.cwd()}/assets/thumb/${filename}-${widthNumber}w-${heightNumber}h.jpg`)
   })
 
-  it('thumbImagePath should take strings and return a path with a width and height', () => {
+  it('expects thumbImagePath(filename, widthString, heightString) to be value', () => {
     expect(utilities.thumbImagePath(filename, widthString, heightString)).toBe(`${process.cwd()}/assets/thumb/${filename}-${widthString}w-${heightString}h.jpg`)
   })
 
-  it('thumbImagePath should take numbers and return a path with a width', () => {
+  it('expects thumbImagePath(filename, widthNumber) to be value', () => {
     expect(utilities.thumbImagePath(filename, widthNumber)).toBe(`${process.cwd()}/assets/thumb/${filename}-${widthNumber}w.jpg`)
   })
 
-  it('thumbImagePath should take strings and return a path with a width and height', () => {
+  it('expects thumbImagePath(filename, widthString) to be value', () => {
     expect(utilities.thumbImagePath(filename, widthString)).toBe(`${process.cwd()}/assets/thumb/${filename}-${widthString}w.jpg`)
   })
 
-  it('thumbImagePath should take numbers and return a path with a height', () => {
+  it('expects thumbImagePath(filename, null, heightNumber) to be value', () => {
     expect(utilities.thumbImagePath(filename, null, heightNumber)).toBe(`${process.cwd()}/assets/thumb/${filename}-${heightNumber}h.jpg`)
   })
 
-  it('thumbImagePath should take strings and return a path with a height', () => {
+  it('expects thumbImagePath(filename, null, heightString) to be value', () => {
     expect(utilities.thumbImagePath(filename, null, heightString)).toBe(`${process.cwd()}/assets/thumb/${filename}-${heightString}h.jpg`)
   })
 
-  it('thumbImagePath should return a path with no width or height', () => {
+  it('expects thumbImagePath(filename) to be value', () => {
     expect(utilities.thumbImagePath(filename)).toBe(`${process.cwd()}/assets/thumb/${filename}.jpg`)
+  })
+})
+
+describe('testing resizeImage', () => {
+  const filename = 'fjord'
+  const widthNumber = 300
+  const widthString = '300'
+  const heightNumber = 200
+  const heightString = '200'
+
+  it('expects resizeImage(filename, widthNumber, heightNumber) to create an image', () => {
+    utilities.resizeImage(filename as string, widthNumber, heightNumber)
+      .then((data) => {
+        expect(data.width).toEqual(widthNumber)
+        expect(data.height).toEqual(heightNumber)
+
+        // remove thumbnail image
+        fsPromises.rm(utilities.thumbImagePath(filename, widthNumber, heightNumber))
+      }) 
+  })
+
+  it('expects resizeImage(filename, widthNumber) to create an image', () => {
+    utilities.resizeImage(filename as string, widthNumber)
+      .then((data) => {
+        expect(data.width).toEqual(widthNumber)
+
+        // remove thumbnail image
+        fsPromises.rm(utilities.thumbImagePath(filename, widthNumber))
+      }) 
+  })
+
+
+  it('expects resizeImage(filename, null, heightNumber) to create an image', () => {
+    utilities.resizeImage(filename as string, null, heightNumber)
+      .then((data) => {
+        expect(data.height).toEqual(heightNumber)
+
+        // remove thumbnail image
+        fsPromises.rm(utilities.thumbImagePath(filename, null, heightNumber))
+      }) 
+  })
+
+  it('expects resizeImage(filename, widthString, heightString) to create an image', () => {
+    utilities.resizeImage(filename as string, widthString, heightString)
+      .then((data) => {
+        expect(data.width).toEqual(parseInt(widthString))
+        expect(data.height).toEqual(parseInt(heightString))
+
+        // remove thumbnail image
+        fsPromises.rm(utilities.thumbImagePath(filename, widthString, heightString))
+      }) 
+  })
+
+  it('expects resizeImage(filename, widthString) to create an image', () => {
+    utilities.resizeImage(filename as string, widthString)
+      .then((data) => {
+        expect(data.width).toEqual(parseInt(widthString))
+
+        // remove thumbnail image
+        fsPromises.rm(utilities.thumbImagePath(filename, widthNumber))
+      }) 
+  })
+
+
+  it('expects resizeImage(filename, null, heightString) to create an image', () => {
+    utilities.resizeImage(filename as string, null, heightString)
+      .then((data) => {
+        expect(data.height).toEqual(parseInt(heightString))
+
+        // remove thumbnail image
+        fsPromises.rm(utilities.thumbImagePath(filename, null, heightString))
+      }) 
   })
 })
