62105edfa0bf61e26523c215783daffbd871bfac
Michael Eisenbraun <=>
2021-08-07 20:56:16 -0400

    Removes test code

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,8 @@ import routes from './routes'
 const app = express()
 const port = 3000
 
-const add = (a:number, b:number) => {
-  return a + b
-}
-
 app.use('/api', routes)
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
 })
-
-export default add
