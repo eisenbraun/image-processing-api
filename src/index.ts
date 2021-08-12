8d37714bcdc3771e1b12659a51ad31ae8d1e817d
Michael Eisenbraun <=>
2021-08-12 10:32:36 -0400

    Updates logging

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express from 'express'
 import routes from './routes'
+// import Debug from 'debug'
+import winston, { format } from 'winston'
 
 const app = express()
 const port = 3000
+// const log = Debug('http')
+const logger = winston.createLogger({
+  level: 'info',
+  format: format.combine(
+    format.colorize(),
+    format.simple()
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+})
 
 app.use('/api', routes)
 
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`)
+  logger.info(`server started at http://localhost:${port}`)
 })
 
 export default app
